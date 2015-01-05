2f528772eab274785a652a7c5748fed47d0deff2
Aule <aule@osanwe.com>
2015-01-04 22:44:33 -0800

    Chapter 4, page 48 - Mongoose models - Troubleshooting why data isn't persisted in mongodb

--- models/posts.js
+++ models/posts.js
@@ -1,5 +1,5 @@
 // is dependent on db.js and creates a schema for the model
-var db = require('.../db')
+var db = require('../db')
 var Post = db.model('Post', {
     username: {type: String, requried:true },
     body: {type: String, required: true },
--- server.js
+++ server.js
@@ -20,7 +20,23 @@ app.get('/api/posts', function (req, res) {
 })
 
 // submits data
-app.post('/api/posts', function (req, res) {
+var Post = require('./models/posts')
+app.post('/api/posts', function (req, res, next) {
+
+    //build a new model of schema username and body
+    var post = new Post({
+        username: req.body.username,
+        body: req.body.body
+    })
+
+    //save model above and send a json representation of it
+    // if there's an error, the next call will pass it to Express to pass on to the client
+    post.save(function (err,post) {
+        if (err) { return next(err) }
+        res.json(201, post)
+    })
+
+    //output post and associated data
     console.log('post received!')
     console.log(req.body.username)
     console.log(req.body.body)
