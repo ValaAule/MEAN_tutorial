1883e4f35138fc0619181a962217a5aea9d90281
Aule <aule@osanwe.com>
2015-01-14 23:45:59 -0800

    Ch 5, page 67 - Still broken, diff error around "Cannot find module .contollers/static' upon startup - Refactored app out of the server.js file

---
+++ assets/app.js
@@ -0,0 +1,36 @@
+/**
+ * Created by whitman on 1/14/15.
+ */
+
+
+//create the app module
+var app = angular.module("app",[])
+
+app.service('PostsSvc', function ($http) {
+    this.fetch = function () {
+        return $http.get('/api/posts')
+    }
+    this.create = function (post) {
+        return $http.post('/api/posts', post)
+    }
+})
+
+//create the PostsCtrl module and dependency inject $scope, the scope ties/glues the controller to the view
+//integrate to the REST API to post back to MongoDB.  Only the success function is posted and it nulls the value for the next time
+app.controller("PostsCtrl",function ($scope, PostsSvc) {
+    $scope.addPost = function() {
+        if ($scope.postBody) {
+            $http.post('/api/posts', {
+                username: 'new user3',
+                body: $scope.postBody
+            }).success(function (post) {
+                $scope.posts.unshift(post)
+                $scope.postBody = null
+            })
+        }
+    }
+
+    PostsSvc.fetch()
+        .success(function (posts) {
+            $scope.posts = posts
+})
--- controllers/static.js
+++ controllers/static.js
@@ -2,8 +2,10 @@
  * Created by Aule on 1/12/15.
  * Delivers the angular app
  */
+var express = require('express')
+var router = express.Router()
 
-var router = require('express').Router()
+router.use(express.static(_dirname + '/../assets'))
 
 router.get('/', function (req, res){
     res.sendfile('layouts/posts.html')
