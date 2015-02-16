6dacb97950ccb957125495333c5499aabffab9b8
Aule <aule@osanwe.com>
2015-02-16 09:46:58 -0700

    Ch 8 (pg 119) - enabled app to work without wifi

--- ng/user.svc.js
+++ ng/user.svc.js
@@ -4,29 +4,30 @@
 
 angular.module('app')
 .service('UserSvc', function($http){
-        var svc = this
-        svc.getUser = function(){
-    return $http.get('/api/users')
-    .then(function (response) {
-      return response.data
-            })
-        }
-        svc.login=function(username, password) {
-            return $http.post('/api/sessions', {
-                username: username, password: password
-    }).then(function (response) {
-      svc.token = response.data
-      $http.defaults.headers.common['X-Auth'] = response.data
-                return svc.getUser()
-    })
-  }
+    var svc = this
+    svc.getUser = function(){
+        return $http.get('/api/users')
+        .then(function (response) {
+            return response.data
+        })
+    }
+
+    svc.login=function(username, password) {
+        return $http.post('/api/sessions', {
+            username: username, password: password
+        }).then(function (response) {
+            window.localStorage.token = response.data
+            $http.defaults.headers.common['X-Auth'] = response.data
+            return svc.getUser()
+        })
+    }
 
  //used to register new users and automatically logs them in
-  svc.register = function (username, password) {
-    return $http.post('/api/users', {
-      username: username, password: password
-    }).then(function () {
-      return svc.login(username, password)
-            })
-            }
-    })
+    svc.register = function (username, password) {
+        return $http.post('/api/users', {
+            username: username, password: password
+        }).then(function () {
+            return svc.login(username, password)
+        })
+    }
+})
