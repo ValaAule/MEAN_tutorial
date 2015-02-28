885322baa397cb59aeecf7c62c47af9166b24f58
Aule <aule@osanwe.com>
2015-02-28 14:22:53 -0800

    Heroku - Attempt to fix unregistered mongoose schema (might want to back this out)

--- controllers/api/posts.js
+++ controllers/api/posts.js
@@ -5,10 +5,11 @@
 
 var router = require('express').Router()
 var websockets = require('../../websockets')
-var Post = require('../../models/post')
+
 
 // get data
 router.get('/api/posts', function (req, res, next) {
+    var Post = require('../../models/post')
     Post.find(function(err, posts) {
         if (err) { return next(err) }
         res.json(posts)
