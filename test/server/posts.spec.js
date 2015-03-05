387458c1fa8c220c1f13a0e4ffb1c37470477b93
Aule <aule@osanwe.com>
2015-03-05 08:43:56 -0800

    Ch 11 (pg 168) - user unit tests

--- test/server/posts.spec.js
+++ test/server/posts.spec.js
@@ -3,7 +3,7 @@
  */
 
 var expect = require('chai').expect
-var ctrl = require('../../../../')
+var ctrl = require('../../controllers/api/posts')
 
 describe('controllers.api.posts', function() {
     it('exists', function() {
---
+++ test/server/posts2.spec.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Aule on 3/5/15.
+ */
+
+var request = require('supertest')
+var express = require('express')
+var app = express()
+
+app.get('/user', function(req,res){
+    res.status(200).send({name: 'mochaUser'})
+})
+
+describe('GET /users', function(){
+    it('responds with proper json', function(done){
+        request(app)
+            .get('/user')
+            .expect('Content-Type', /json/)
+            .expect({name:'mochaUser'}, done)
+    })
+})
